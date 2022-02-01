7ee5a128aeda8fe81aa9d2916fbef2402372429e
Shamar Stewart <jayhacker19@gmail.com>
2022-02-01 15:38:41 -0500

    new changes made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import propTypes from 'prop-types';
+
+// css
 import './App.css';
+import './CarTable.css';
+
+// json file
+import Cars from './cars.json'
+
+
+const Header = () => (
+  <header>
+    <div className="nav-wrapper">
+      <div className="logo-container">
+        <h1 className='logo'>LOGO</h1>
+      </div>
+      <nav>
+        <input className="hidden" type="checkbox" id="menuToggle" />
+        <label className="menu-btn" htmlFor="menuToggle">
+          <div className="menu"></div>
+          <div className="menu"></div>
+          <div className="menu"></div>
+        </label>
+        <div className="nav-container">
+          <ul className="nav-tabs">
+            <li className="nav-tab">Home</li>
+            <li className="nav-tab">Products</li>
+            <li className="nav-tab">Services</li>
+            <li className="nav-tab">FAQ</li>
+            <li className="nav-tab">Contact</li>
+            <li className="nav-tab">Careers</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </header>
+);
+
+const CarsRow = ({ Cars }) => (
+  <tr>
+    <td>{Cars.Name.toUpperCase()}</td>
+    <td>{Cars.Year}</td>
+    <td>{Cars.Origin}</td>
+  </tr>
+);
+
+CarsRow.propTypes = {
+  Cars: propTypes.shape({
+    Name: propTypes.string
+  }),
+}
 
 function App() {
+
+  const [search, setSearch] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {Header && (
+        <Header />
+      )}
+      <div className="center">
+        <h1>Search For Your Dream Car</h1>
+      </div>
+      <div>
+        <div className='search-box'>
+          <div className='center-serch-box'>
+            <input />
+          </div>
+        </div>
+        <div className='display-section'>
+          <table className="CarTable">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Year</th>
+                <th>Origin</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Cars.slice(0, 10).map((Cars) => (
+                <CarsRow key={Cars.ID} Cars={Cars} />
+              ))}
+              </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
